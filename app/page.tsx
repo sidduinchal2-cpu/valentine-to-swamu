034fd037d6291f50d22d5460cb665b216e7b9c14
sidduinchal2-cpu <sidduinchal2@gmail.com>
2026-02-03 18:41:10 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "28px",
+        fontWeight: "bold",
+        backgroundColor: "#ffc0cb",
+      }}
+    >
+      💖 Valentine Website is LIVE 💖
+    </div>
+  );
+}
